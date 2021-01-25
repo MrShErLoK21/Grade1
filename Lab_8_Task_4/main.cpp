64e7dbe78314d6add0305195be2b856b6ee8db86
repl.it user <replituser@example.com>
2021-01-25 14:15:32 +0000

    statement add

--- Lab_8_Task_4/main.cpp
+++ Lab_8_Task_4/main.cpp
@@ -13,12 +13,23 @@ struct note
 
 int main() 
 {
-  bool want_to_insert = false;
-  while (want_to_insert == false)
+  bool statement = false;
+  string answer;
+  while (statement == false)
   {
-    cout<<"Хотите ли вы дополнить данные в файле?"<<endl;
-    
-    
+    cout<<"Хотите ли вы дополнить данные в файле? (1/0)"<<endl;
+    cin >> answer;
+    if(answer == "1")
+    {
+        statement = true;
+        statement = true;
+    }   
+    else if (answer == "0")
+    {
+        statement = true;
+        statement = false;
+    }
   }
+  cout << answer;
   
 }
