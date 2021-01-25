64a668ce9212381790ffb5679544cd68635663e3
repl.it user <replituser@example.com>
2021-01-25 13:55:36 +0000

    add new task 4

---
+++ Lab_8_Task_2/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string filename;
+    cout << "Введите путь к файлу: ";
+    cin >> filename;
+    fstream fin(filename);
+    string fullText = "";
+    if (!fin.is_open())
+    {
+        cout << "Файл не может быть открыт или создан\n";
+    }
+    else
+    {
+        string s1;
+        string s2;
+        cout << "Введите первое слово" << endl;
+        cin >> s1;
+        cout << "Введите второе слово" << endl;
+        cin >> s2;
+        string myText;
+        while (getline(fin, myText))
+        {
+            fullText += myText;
+        }
+        size_t po = fullText.find(s1);
+        if (po != std::string::npos)
+        {
+            fullText.erase(po, s1.length());
+            fullText.insert(po, s2);
+        }
+    }
+    fin.close();
+    ofstream MyFile(filename);
+    MyFile << fullText;
+    MyFile.close();
+    return 0;
+}
---
+++ Lab_8_Task_4/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+struct note_1
+{
+  string first_name;
+  string last_name;
+  long phone_number;
+  int day;
+  int month;
+  int year;
+};
+
+void printf(note_1 lex)
+{
+  
+}
+
+int main() 
+{
+  cout<<"Hello world"<<endl;
+}
