981bf25ae3920928e40a0bcc6b2463d4ff62eb8a
Maksym Lisniak <maxim.l.forstudy@gmail.com>
2021-01-26 19:58:00 +0200

    add some logic to csv read|NOT FINISHED

--- Lab_8_Task_4/main.cpp
+++ Lab_8_Task_4/main.cpp
@@ -1,21 +1,90 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
-#include "util.cpp"
-using namespace std;
-struct note
+#include <iterator>
+#include <string>
+#include <string.h>
+#include <algorithm>
+// #include <boost/algorithm/string.hpp>
+/*
+ * A class to read data from a csv file.
+ */
+struct Note
 {
-  string first_name;
-  string last_name;
-  long phone_number;
-  int day;
-  int month;
-  int year;
+    std::string first_name;
+    std::string last_name;
+    long phone_number;
+    int day;
+    int month;
+    int year;
 };
 
-
-int main() 
+class CSVReader
 {
-  string answer;
-  answer = question();
-  cout<<answer<<endl;
+    std::string fileName;
+    std::string delimeter;
+
+public:
+    CSVReader(std::string filename, std::string delm = ",") : fileName(filename), delimeter(delm)
+    {
+    }
+    // Function to fetch data from a CSV File
+    std::vector<std::vector<std::string>> getData();
+};
+/*
+* Parses through csv file line by line and returns the data
+* in vector of vector of strings.
+*/
+std::vector<std::vector<std::string>> CSVReader::getData()
+{
+    std::ifstream file(fileName);
+    std::vector<std::vector<std::string>> dataList;
+    std::string line = "";
+    // Iterate through each line and split the content using delimeter
+    while (getline(file, line))
+    {
+        std::vector<std::string> vec;
+
+        int n = line.length();
+        // declaring character array
+        char char_array[n + 1];
+
+        // copying the contents of the
+        // string to char array
+        strcpy(char_array, line.c_str());
+
+        char *p = strtok(char_array, ",");
+        while (p)
+        {
+            std::string word = p;
+            vec.push_back(word);
+            p = strtok(NULL, " ");
+        }
+        // boost::algorithm::split(vec, line, boost::is_any_of(delimeter));
+        dataList.push_back(vec);
+    }
+    // Close the File
+    file.close();
+    return dataList;
+}
+int main()
+{
+    // string answer;
+    // answer = question();
+    // cout<<answer<<endl;
+
+    // Creating an object of CSVWriter
+    CSVReader reader("example.csv");
+    // Get the data from CSV File
+    std::vector<std::vector<std::string>> dataList = reader.getData();
+    // Print the content of row by row on screen
+    for (std::vector<std::string> vec : dataList)
+    {
+        for (std::string data : vec)
+        {
+            std::cout << data << " , ";
+        }
+        std::cout << std::endl;
+    }
+    return 0;
 }
