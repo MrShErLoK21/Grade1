ef35855d0d5f6b4585588d201176ecac06e13057
Sherlock Holmes <romachuiko76@gmail.com>
2021-01-28 15:17:35 +0200

    create body

---
+++ RGZ_2/function.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+
+using namespace std;
+
+string question()
+{
+    string answer;
+    bool status = false;
+    while (status == false)
+    {
+        cout<<"Choose function to do. 1 - add new data; 2 - edit data; 3 - amount of data; 4 - identification of situations; 5 - top 10 website; 6 - most downloaded data;"<<endl;
+        cin >> answer;
+        if ( answer == "1")
+        {
+            status = true;
+        }
+        else if (answer == "2")
+        {
+            status = true;
+        }
+        else if (answer == "3")
+        {
+            status = true;
+        }
+        else if (answer == "4")
+        {
+            status = true;
+        }
+        else if (answer == "5")
+        {
+            status = true;
+        }
+        else if (answer == "6")
+        {
+            status = true;
+        }
+    }
+    return answer;
+}
+void add_to_data()
+{
+
+}
+void edit_data()
+{
+
+}
+void amount_of_data()
+{
+
+}
+void situation()
+{
+
+}
+void top_10()
+{
+
+}
+void download()
+{
+
+}
---
+++ RGZ_2/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include "function.cpp"
+#include "csv.cpp"
+
+using namespace std;
+struct proxy_row_values
+{
+    string ip_adress;
+    string login;
+    string website;
+    int days;
+    int months;
+    int years;
+    int hours;
+    int minutes;
+    int seconds;
+    int ammount;
+};
+
+int main(int argc, char* argv[])
+{
+    string path;
+    if (argc == 2)
+    {
+        path = argv[1];
+    }
+  else
+  {
+    cout<< "Incorect syntax. Use ./main file_path"<<endl;
+    return -1;
+  }
+    vector<proxy_row_values> proxy_file_data;
+    ifstream proxy_file(path);
+
+    string answer;
+    answer = question();
+    if (answer == "1")
+    {
+        add_to_data();
+    }
+    else if (answer == "2")
+    {
+        edit_data();
+    }
+    else if (answer == "3")
+    {
+        amount_of_data();
+    }
+    else if(answer == "4")
+    {
+        situation();
+    }
+    else if (answer == "5")
+    {
+        top_10();
+    }
+    else if (answer == "6")
+    {
+        download();
+    }
+    return 0;
+}
